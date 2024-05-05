caf63296d5580be5a6c076d8a1ac530b4dbc198b
AleksKhrap <112804992+AleksKhrap@users.noreply.github.com>
2024-05-05 23:55:58 +0300

    Add main and login pages. Add locators

---
+++ pages/locators.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.common.by import By
+
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+
+class MainPageLocators:
+    pass
+
+
+class LoginPageLocators:
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+    EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REPEAT_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "#register_form button[type='submit']")
+
+
+
