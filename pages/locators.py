1d2347d57812d3c938a15679734119715e51c4fd
AleksKhrap <112804992+AleksKhrap@users.noreply.github.com>
2024-05-06 00:15:30 +0300

    Add locators for product and basket pages

--- pages/locators.py
+++ pages/locators.py
@@ -20,4 +20,16 @@ class LoginPageLocators:
     REGISTER_BUTTON = (By.CSS_SELECTOR, "#register_form button[type='submit']")
 
 
+class ProductPageLocators:
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form button[type='submit']")
+    ALERT_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages div:nth-child(1) .alertinner strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_page .product_main h1")
+    ALERT_PRODUCT_COST = (By.CSS_SELECTOR, "#messages div:nth-child(3) .alertinner p strong")
+    PRODUCT_COST = (By.CSS_SELECTOR,  ".product_main .price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages .alert-success")
 
+
+class BasketPageLocators:
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".basket-mini a")
+    EMPTY_MESSAGE = (By.CSS_SELECTOR, "#content_inner > p")
+    BASKET_ITEMS = (By.CSS_SELECTOR, ".basket-items")
